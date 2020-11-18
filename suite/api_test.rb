4e7c43745ffffceb8642a8e857674ef7f59e73be
Ezekiel Clark <clarkzeke@gmail.com>
2020-11-18 08:59:26 -0700

    Add test to make sure there are no duplicates in the first 5 pages of search results. Adjust year format assertion to take series into account. Update values assertion to assert instance of a string.

--- suite/api_test.rb
+++ suite/api_test.rb
@@ -24,11 +24,19 @@ class ApiTest < Minitest::Test
     results = last_response.obj["Search"]
     keys = ["Title", "Year", "imdbID", "Type", "Poster"]
     results.each do |result|
-      assert_equal keys, result.keys
+      keys.each {|key| assert result.keys.include?(key)}
+      result.values.each {|value| assert_instance_of String, value}
       assert result["Title"].include?("Thomas")
-      assert_equal "movie", result["Type"]
-      year = result["Year"].to_i
-      assert (year > 1900 && year < 2030)
+      yr_length = (result["Year"].length)
+      if yr_length < 6
+        year = result["Year"][0...4].to_i
+        assert (year > 1900 && year < 2030)
+      else
+        start_year = result["Year"][0...4].to_i
+        end_year = result["Year"][-4...yr_length].to_i
+        assert (start_year > 1900 && start_year < 2030)
+        assert (end_year > 1900 && end_year < 2030)
+      end
     end
   end
 
@@ -52,5 +60,21 @@ class ApiTest < Minitest::Test
       request("GET", "", {}, result["Poster"])
       assert_equal 200, last_response.status
     end
+  end
+
+  def test_no_duplicate_search_results_in_first_5_pages
+    request("GET", "?s=friends&apikey=#{ENV["OMDB_API_KEY"]}", {}, ENV["OMDB_BASE_URL"])
+    results = last_response.obj["Search"]
+    page = 1
+    five_page_results = []
+    until page == 6
+      request("GET", "?s=friends&page=#{page}&apikey=#{ENV["OMDB_API_KEY"]}", {}, ENV["OMDB_BASE_URL"])
+      page += 1
+      five_page_results << last_response.obj["Search"]
+    end
+    five_page_results = five_page_results.flatten
+
+    assert_equal 50, five_page_results.length
+    assert_equal 50, five_page_results.uniq.length
   end
 end
