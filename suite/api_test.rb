43c3e73662906483f777cc1a302691c2bce160dd
Ezekiel Clark <clarkzeke@gmail.com>
2020-11-18 07:07:04 -0700

    Set up environment variables. Begin thomas search test.

--- suite/api_test.rb
+++ suite/api_test.rb
@@ -6,15 +6,22 @@ require 'minitest/pride'
 class ApiTest < Minitest::Test
   include RequestHelper
 
+  def setup
+  end
+
   def test_no_api_key
-    request('GET', '?s=star', {}, 'http://www.omdbapi.com/')
-    puts last_response.obj
+    request('GET', '?s=star', {}, ENV["OMDB_BASE_URL"])
     expected_response = "False"
     expected_error = "No API key provided."
-    
+
     # TODO: Task 2 - add the assertion
     assert_equal expected_response, last_response.obj["Response"]
     assert_equal expected_error, last_response.obj["Error"]
+  end
+
+  def test_api_search_function_works_as_expected
+    request("GET", "?s=thomas&apikey=#{ENV["OMDB_API_KEY"]}", {}, ENV["OMDB_BASE_URL"])
+    require "pry"; binding.pry
   end
 
 end
