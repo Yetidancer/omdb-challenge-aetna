1899b9a4ecf5e30fb0f47e34d3082ed438aecfe6
Ezekiel Clark <clarkzeke@gmail.com>
2020-11-17 17:42:41 -0700

    Add string variables for expected response and error. Add assertions for those expected responses.

--- suite/api_test.rb
+++ suite/api_test.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../support/test_helper', __dir__)
 
 require 'minitest/autorun'
+require 'minitest/pride'
 
 class ApiTest < Minitest::Test
   include RequestHelper
@@ -8,7 +9,12 @@ class ApiTest < Minitest::Test
   def test_no_api_key
     request('GET', '?s=star', {}, 'http://www.omdbapi.com/')
     puts last_response.obj
-
+    expected_response = "False"
+    expected_error = "No API key provided."
+    
     # TODO: Task 2 - add the assertion
+    assert_equal expected_response, last_response.obj["Response"]
+    assert_equal expected_error, last_response.obj["Error"]
   end
+
 end
