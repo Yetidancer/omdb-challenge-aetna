82daa46c7d69dd4046d4dea8dd1d28f8436c2ac5
Ezekiel Clark <clarkzeke@gmail.com>
2020-11-18 11:56:46 -0700

    Update variables to be more specific. Update assertions to accurately assess request response.

--- suite/api_test.rb
+++ suite/api_test.rb
@@ -7,6 +7,7 @@ class ApiTest < Minitest::Test
   include RequestHelper
 
   def setup
+    @search_term = "thomas"
   end
 
   def test_no_api_key
@@ -14,20 +15,20 @@ class ApiTest < Minitest::Test
     expected_response = "False"
     expected_error = "No API key provided."
 
-    # TODO: Task 2 - add the assertion
     assert_equal expected_response, last_response.obj["Response"]
     assert_equal expected_error, last_response.obj["Error"]
   end
 
   def test_api_search_function_works_as_expected
-    request("GET", "?s=thomas&apikey=#{ENV["OMDB_API_KEY"]}", {}, ENV["OMDB_BASE_URL"])
+    request("GET", "?s=#{@search_term}&apikey=#{ENV["OMDB_API_KEY"]}", {}, ENV["OMDB_BASE_URL"])
     results = last_response.obj["Search"]
-    keys = ["Title", "Year", "imdbID", "Type", "Poster"]
+    expected_keys = ["Title", "Year", "imdbID", "Type", "Poster"]
 
     results.each do |result|
-      keys.each {|key| assert result.keys.include?(key)}
+      expected_keys.each {|expected_key| assert result.keys.include?(expected_key)}
       result.values.each {|value| assert_instance_of String, value}
-      assert result["Title"].include?("Thomas")
+      lowercase_title = result["Title"].downcase
+      assert lowercase_title.include?(@search_term)
 
       yr_length = (result["Year"].length)
       if yr_length < 6
@@ -43,47 +44,43 @@ class ApiTest < Minitest::Test
   end
 
   def test_api_search_function_returns_imdb_accessible_results
-    request("GET", "?s=thomas&apikey=#{ENV["OMDB_API_KEY"]}", {}, ENV["OMDB_BASE_URL"])
+    request("GET", "?s=#{@search_term}&apikey=#{ENV["OMDB_API_KEY"]}", {}, ENV["OMDB_BASE_URL"])
     results = last_response.obj["Search"]
 
     results.each do |result|
-      imdb_id =  result["imdbID"]
+      imdb_id = result["imdbID"]
       request("GET", "?i=#{imdb_id}&apikey=#{ENV["OMDB_API_KEY"]}", {}, ENV["OMDB_BASE_URL"])
-      assert last_response.obj["Response"]
+      assert_equal "True", last_response.obj["Response"]
     end
   end
 
   def test_all_search_results_poster_links_work
-    request("GET", "?s=thomas&apikey=#{ENV["OMDB_API_KEY"]}", {}, ENV["OMDB_BASE_URL"])
+    request("GET", "?s=#{@search_term}&apikey=#{ENV["OMDB_API_KEY"]}", {}, ENV["OMDB_BASE_URL"])
     results = last_response.obj["Search"]
 
     results.each do |result|
-      assert_equal "http", result["Poster"][0...4]
+      assert result["Poster"].include?(".com")
       request("GET", "", {}, result["Poster"])
       assert_equal 200, last_response.status
     end
   end
 
   def test_no_duplicate_search_results_in_first_5_pages
-    request("GET", "?s=thomas&apikey=#{ENV["OMDB_API_KEY"]}", {}, ENV["OMDB_BASE_URL"])
-    results = last_response.obj["Search"]
     page = 1
     five_page_results = []
     until page == 6
-      request("GET", "?s=friends&page=#{page}&apikey=#{ENV["OMDB_API_KEY"]}", {}, ENV["OMDB_BASE_URL"])
+      request("GET", "?s=#{@search_term}&page=#{page}&apikey=#{ENV["OMDB_API_KEY"]}", {}, ENV["OMDB_BASE_URL"])
+      last_response.obj["Search"].each {|result| five_page_results << result["imdbID"]}
       page += 1
-      five_page_results << last_response.obj["Search"]
     end
-    five_page_results = five_page_results.flatten
 
     assert_equal 50, five_page_results.length
     assert_equal 50, five_page_results.uniq.length
   end
 
-  def test_there_are_less_than_30_movie_titles_containing_the_word_squash
-    request("GET", "?s=squash&apikey=#{ENV["OMDB_API_KEY"]}", {}, ENV["OMDB_BASE_URL"])
+  def test_there_are_less_than_20_movie_titles_containing_the_word_squash
+    request("GET", "?s=squash&type=movie&apikey=#{ENV["OMDB_API_KEY"]}", {}, ENV["OMDB_BASE_URL"])
     number_of_results = last_response.obj["totalResults"].to_i
-
-    assert number_of_results < 30
+    assert number_of_results < 20
   end
 end
